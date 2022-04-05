563d7457ca862d3f3031e6380e673f2b77e3a394
Lukas Zöllner <lks.zoellner@gmail.com>
2022-04-05 12:57:17 +0200

    add DoublyLinkedList

---
+++ src/data-structures/doubly-linked-list.test.ts
@@ -0,0 +1,124 @@
+import DoublyLinkedList from "./doubly-linked-list";
+
+let linkedList: DoublyLinkedList<unknown>;
+beforeEach(() => (linkedList = new DoublyLinkedList()));
+
+describe("The constructor", () => {
+  test("should create an empty list given no parameters", () => {
+    expect(new DoublyLinkedList().length).toBe(0);
+  });
+  test("should create a populated list given an iterable", () => {
+    let populatedList = new DoublyLinkedList(["a", "b", "c"]);
+    linkedList.addAtIndex(0, "a");
+    linkedList.addAtIndex(1, "b");
+    linkedList.addAtIndex(2, "c");
+    expect(populatedList).toEqual(linkedList);
+  });
+});
+
+test("should return -1 if the index is invalid", () => {
+  linkedList.addAtTail(1);
+  expect(linkedList.get(10)).toBe(-1);
+  expect(linkedList.get(-2)).toBe(-1);
+});
+
+test("can get the last value of a one node list", () => {
+  linkedList.addAtTail(7);
+  expect(linkedList.get(0)).toBe(7);
+});
+
+test("can get the last value of a two node list", () => {
+  linkedList.addAtTail(7);
+  linkedList.addAtTail(2);
+  expect(linkedList.get(1)).toBe(2);
+});
+
+test("can get the first value of a two node list", () => {
+  linkedList.addAtTail(7);
+  linkedList.addAtTail(2);
+  expect(linkedList.get(0)).toBe(7);
+});
+
+test("can add a value to the head of an empty list", () => {
+  linkedList.addAtHead(7);
+  expect(linkedList.get(0)).toBe(7);
+});
+
+test("can add a value to the head of a one node list list", () => {
+  linkedList.addAtHead(7);
+  linkedList.addAtHead(3);
+
+  expect(linkedList.get(0)).toBe(3);
+  expect(linkedList.get(1)).toBe(7);
+});
+
+test("can get the ith value counting from the end of the list by passing a negative number", () => {
+  linkedList.addAtHead("b");
+  linkedList.addAtHead("a");
+  linkedList.addAtTail("c");
+
+  expect(linkedList.get(-1)).toBe("c");
+  expect(linkedList.get(-2)).toBe("b");
+  expect(linkedList.get(-3)).toBe("a");
+});
+
+describe("addAtIndex(index, value)", () => {
+  test("can add a value to an empty list", () => {
+    linkedList.addAtIndex(0, 7);
+    expect(linkedList.get(0)).toBe(7);
+  });
+  test("can add a value to the start of a list", () => {
+    linkedList.addAtIndex(0, "b");
+    linkedList.addAtIndex(0, "a");
+    expect(linkedList.get(0)).toBe("a");
+  });
+  test("can add a value to the end of a list", () => {
+    linkedList.addAtIndex(0, "a");
+    linkedList.addAtIndex(1, "b");
+    expect(linkedList.get(1)).toBe("b");
+  });
+  test("can add a value in the middle of a list", () => {
+    linkedList.addAtIndex(0, "a");
+    linkedList.addAtIndex(1, "c");
+    linkedList.addAtIndex(1, "b");
+    expect(linkedList.get(1)).toBe("b");
+  });
+  test("should ignore value if index is invalid", () => {
+    let emptyList = new DoublyLinkedList();
+    linkedList.addAtIndex(10, "a");
+    expect(linkedList).toEqual(emptyList);
+  });
+});
+
+describe("deleteAtIndex(index)", () => {
+  test("should delete the only node of a single node list", () => {
+    let emptyList = new DoublyLinkedList();
+    linkedList.addAtHead(1);
+    linkedList.deleteAtIndex(0);
+    expect(linkedList).toEqual(emptyList);
+  });
+  test("should delete the first node of a list", () => {
+    let linkedList = new DoublyLinkedList(["a", "b"]);
+    linkedList.deleteAtIndex(0);
+    let expectedList = new DoublyLinkedList(["b"]);
+    expect(linkedList).toEqual(expectedList);
+  });
+  test("should delete the last node of a list", () => {
+    let linkedList = new DoublyLinkedList(["a", "b"]);
+    linkedList.deleteAtIndex(1);
+    let expectedList = new DoublyLinkedList(["a"]);
+    expect(linkedList).toEqual(expectedList);
+  });
+  test("should delete a middle node of a list", () => {
+    let linkedList = new DoublyLinkedList(["a", "b", "c"]);
+    linkedList.deleteAtIndex(1);
+    let expectedList = new DoublyLinkedList(["a", "c"]);
+    expect(linkedList).toEqual(expectedList);
+  });
+  test("should do nothing if index is invalid", () => {
+    let expectedList = new DoublyLinkedList(["a"]);
+    linkedList.addAtHead("a");
+    linkedList.deleteAtIndex(3);
+    expect(linkedList).toEqual(expectedList);
+  });
+});
---
+++ src/data-structures/doubly-linked-list.ts
@@ -0,0 +1,114 @@
+class DoublyLinkedListNode<T> {
+  prev: DoublyLinkedListNode<T> | null;
+  next: DoublyLinkedListNode<T> | null;
+  val: T;
+  constructor(val: T) {
+    this.prev = null;
+    this.next = null;
+    this.val = val;
+  }
+}
+
+export default class DoublyLinkedList<T> {
+  length: number;
+  head: DoublyLinkedListNode<T> | null;
+  tail: DoublyLinkedListNode<T> | null;
+
+  constructor(iterable?: Iterable<T>) {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+
+    if (iterable) {
+      for (let element of iterable) {
+        this.addAtTail(element);
+      }
+    }
+  }
+
+  addAtTail(val: T) {
+    let node = new DoublyLinkedListNode(val);
+    if (this.tail) {
+      this.tail.next = node;
+    } else {
+      this.head = node;
+    }
+    node.prev = this.tail;
+    this.tail = node;
+    this.length++;
+  }
+
+  addAtHead(val: T) {
+    if (!this.head) {
+      this.addAtTail(val);
+    } else {
+      let node = new DoublyLinkedListNode(val);
+      node.next = this.head;
+      this.head.prev = node;
+      this.head = node;
+      this.length++;
+    }
+  }
+
+  addAtIndex(index: number, val: T) {
+    if (index === 0) return this.addAtHead(val);
+    if (index === this.length) return this.addAtTail(val);
+    if (!this.isValidIndex(index)) return;
+    let next = this.getNode(index);
+    let prev = next.prev as DoublyLinkedListNode<T>;
+    let node = new DoublyLinkedListNode(val);
+    node.prev = prev;
+    node.next = next;
+    prev.next = node;
+    next.prev = node;
+    this.length++;
+  }
+
+  get(index: number) {
+    if (!this.isValidIndex(index)) return -1;
+    return this.getNode(index).val;
+  }
+
+  deleteAtIndex(index: number): void {
+    if (!this.isValidIndex(index)) return;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+      return;
+    }
+    let node = this.getNode(index);
+    if (node === this.head) {
+      this.head = this.head.next;
+      this.head!.prev = null;
+    } else if (node === this.tail) {
+      this.tail = this.tail.prev;
+      this.tail!.next = null;
+    } else {
+      node.prev!.next = node.next;
+      node.next!.prev = node.prev;
+    }
+
+    this.length--;
+  }
+
+  private isValidIndex(index: number) {
+    return index < this.length && index * -1 <= this.length;
+  }
+
+  private getNode(index: number): DoublyLinkedListNode<T> {
+    let node;
+    if (index < 0) {
+      node = this.tail as DoublyLinkedListNode<T>;
+      for (let i = -1; i > index; i--) {
+        node = node?.prev;
+      }
+    } else {
+      node = this?.head;
+      for (let i = 0; i < index; i++) {
+        node = node?.next;
+      }
+    }
+    return node as DoublyLinkedListNode<T>;
+  }
+}
