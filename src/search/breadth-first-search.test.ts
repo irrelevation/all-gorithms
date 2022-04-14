6fa24b3cfe433922876ea52c16d6010fe71276a8
Lukas Zöllner <lks.zoellner@gmail.com>
2022-04-14 12:45:25 +0200

    fix find and distance method not keeping track of visited nodes

--- src/search/breadth-first-search.test.ts
+++ src/search/breadth-first-search.test.ts
@@ -4,41 +4,64 @@ import { BFS } from "./breadth-first-search";
 import Node from "../data-structures/node";
 import { Node as TNode } from "../types";
 
-// describe("The distance(from: Node, to: Node) function", () => {
-//     test("should return 0 if the nodes are identical", () => {
-//         expect
-//     })
-// });
+const isStart = (node: TNode<unknown>) => node.value === "start";
+const isTarget = (node: TNode<unknown>) => node.value === "target";
+const isFalse = (node: TNode<unknown>) => false;
+let start: TNode<unknown>;
+let target: TNode<unknown>;
+beforeEach(() => {
+  start = new Node("start");
+  target = new Node("target");
+});
+
+describe("The distance(start, testFn) method", () => {
+  test("should return 0 if we are looking for the start node", () => {
+    expect(BFS.distance(start, isStart)).toBe(0);
+  });
+  test("should return null if there is no node that satisfies the test function", () => {
+    expect(BFS.distance(start, isFalse)).toBe(null);
+  });
+  test("should return 1 if the target is a neighbour", () => {
+    start.neighbours = [target];
+    expect(BFS.distance(start, isTarget)).toBe(1);
+  });
+  test("should return 2 if the target is a neighbours neighbour", () => {
+    let neighbour = new Node("neighbour", [target]);
+    start.neighbours = [neighbour];
+    expect(BFS.distance(start, isTarget)).toBe(2);
+  });
+  test("should return null if the target is not reachable but the is a cycle in the graph", () => {
+    start.neighbours = [target];
+    target.neighbours = [start];
+    expect(BFS.distance(start, isFalse)).toBe(null);
+  });
+});
 
 describe("The find(start, testFn) method", () => {
   test("should return the start node if it satisfies the test function", () => {
-    const startNode = new Node();
-    const isStartNode = (node: TNode<unknown>) => node === startNode;
-    expect(BFS.find(startNode, isStartNode)).toBe(startNode);
+    expect(BFS.find(start, isStart)).toBe(start);
   });
   test("should return null if the start node doesnt have neighbours and doesn't satisfies the test function", () => {
-    const startNode = new Node();
-    const isFalse = (node: TNode<unknown>) => false;
-    expect(BFS.find(startNode, isFalse)).toBe(null);
+    expect(BFS.find(start, isFalse)).toBe(null);
   });
   test("should return the target node, if the start node doesnt have satisfies the test function, but the target does and is one of the neighbours", () => {
-    const targetNode = new Node("target");
-    const startNode = new Node("start", [targetNode]);
-    const isTarget = (node: TNode<unknown>) => node.value === "target";
-    expect(BFS.find(startNode, isTarget)).toBe(targetNode);
+    start.neighbours = [target];
+    expect(BFS.find(start, isTarget)).toBe(target);
   });
   test("should return the target node if it is a neighbours neighbour and satisfies the test function", () => {
-    const targetNode = new Node("target");
-    const neighbour = new Node("neighbour", [targetNode]);
-    const startNode = new Node("start", [neighbour]);
-    const isTarget = (node: TNode<unknown>) => node.value === "target";
-    expect(BFS.find(startNode, isTarget)).toBe(targetNode);
+    const neighbour = new Node("neighbour", [target]);
+    start.neighbours = [neighbour];
+    expect(BFS.find(start, isTarget)).toBe(target);
   });
   test("should return null if the graph has a cycle and there is no node that satisfies the condition", () => {
-    const start = new Node("start");
-    const B = new Node("B", [start]);
-    start.neighbours = [B];
-    const isFalse = (node: TNode<unknown>) => false;
+    start.neighbours = [target];
+    target.neighbours = [start];
+    expect(BFS.find(start, isFalse)).toBe(null);
+  });
+  test("should return null if the graph has a cycle deeper down the graph and there is no node that satisfies the condition", () => {
+    let next = new Node("next", [target])
+    start.neighbours = [next];
+    target.neighbours = [next];
     expect(BFS.find(start, isFalse)).toBe(null);
   });
 });
--- src/search/breadth-first-search.ts
+++ src/search/breadth-first-search.ts
@@ -2,20 +2,40 @@ import { Node } from "../types";
 
 type TestFunction<T> = (node: Node<T>) => boolean;
 export const BFS = {
-  distance<T>(from: Node<T>, to: Node<T>): number {
-    return -1;
-  },
-  find<T>(start: Node<T>, test: TestFunction<T>): Node<T> | null {
-    if (test(start)) return start;
-    let queue = [...start.neighbours];
+  findNodeAndDistance<T>(
+    start: Node<T>,
+    test: TestFunction<T>
+  ): { node: Node<T> | null; distance: number | null } {
+    if (test(start)) return { node: start, distance: 0 };
+    let queue = [...start.neighbours].map((node) => {
+      return { node, distance: 1 };
+    });
     let visited = new Set();
     visited.add(start);
     while (queue.length > 0) {
-      const node = queue.shift() as Node<T>;
+      const { node, distance } = queue.shift() as {
+        node: Node<T>;
+        distance: number;
+      };
       if (visited.has(node)) continue;
-      if (test(node)) return node;
-      queue.push(...node.neighbours);
+
+      if (test(node)) return { node, distance };
+      queue.push(
+        ...node.neighbours.map((node) => {
+          return { node, distance: distance + 1 };
+        })
+      );
+      visited.add(node);
     }
-    return null;
+
+    return { node: null, distance: null };
+  },
+  distance<T>(start: Node<T>, test: TestFunction<T>): number | null {
+    const { distance } = this.findNodeAndDistance(start, test);
+    return distance;
+  },
+  find<T>(start: Node<T>, test: TestFunction<T>): Node<T> | null {
+    const { node } = this.findNodeAndDistance(start, test);
+    return node;
   },
 };
